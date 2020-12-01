673914aa0faff19309b1ebdbfc45e15eaafe9368
David Rose <doppler@gmail.com>
2020-12-01 16:28:28 -0500

    add abortController

--- src/components/WindsAloft.tsx
+++ src/components/WindsAloft.tsx
@@ -25,43 +25,56 @@ const WindsAloft: React.FC = () => {
   }, []);
 
   useEffect(() => {
+    const abortController = new AbortController();
     const fetchElevationData = async (location: typeof InitialLocation) => {
       if (elevation || !location.latitude) return;
-      setStatus('Determining location elevation...');
-      const queryStr = Object.entries({
-        x: location.longitude,
-        y: location.latitude,
-        units: 'Meters',
-        output: 'json',
-      })
-        .map(pair => pair.join('='))
-        .join('&');
-      const url = `https://nationalmap.gov/epqs/pqs.php?${queryStr}`;
-      const response = await fetch(url);
-      const json = await response.json();
-      setElevation(
-        json.USGS_Elevation_Point_Query_Service.Elevation_Query.Elevation
-      );
-      setStatus('');
+      try {
+        setStatus('Determining location elevation...');
+        const queryStr = Object.entries({
+          x: location.longitude,
+          y: location.latitude,
+          units: 'Meters',
+          output: 'json',
+        })
+          .map(pair => pair.join('='))
+          .join('&');
+        const url = `https://nationalmap.gov/epqs/pqs.php?${queryStr}`;
+        const response = await fetch(url, { signal: abortController.signal });
+        const json = await response.json();
+        setElevation(
+          json.USGS_Elevation_Point_Query_Service.Elevation_Query.Elevation
+        );
+        setStatus('');
+      } catch (e) {
+        console.error(e.name);
+      }
     };
     fetchElevationData(location);
+
+    return () => abortController.abort();
   }, [location, elevation]);
 
   useEffect(() => {
+    const abortController = new AbortController();
     const fetchWindsAloftData = async (
       location: typeof InitialLocation,
       elevation: number
     ) => {
-      if (forecastJSON || !elevation) return;
-      setStatus('Fetching winds aloft forecast data...');
-      const url = `https://weatherflow-dash.herokuapp.com/winds-aloft/${location.latitude}/${location.longitude}/${elevation}`;
-      const response = await fetch(url, { mode: 'cors' });
-      const json = await response.json();
-      setForecastJSON(json);
-      setStatus('');
+      try {
+        if (forecastJSON || !elevation) return;
+        setStatus('Fetching winds aloft forecast data...');
+        const url = `https://weatherflow-dash.herokuapp.com/winds-aloft/${location.latitude}/${location.longitude}/${elevation}`;
+        const response = await fetch(url, { signal: abortController.signal });
+        const json = await response.json();
+        setForecastJSON(json);
+        setStatus('');
+      } catch (e) {
+        console.error(e.name);
+      }
     };
-
     fetchWindsAloftData(location, elevation);
+
+    return () => abortController.abort();
   }, [location, elevation, forecastJSON]);
 
   return forecastJSON ? (
