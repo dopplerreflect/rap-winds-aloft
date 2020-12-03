28a1869e65d5284ca317ca2323897d895a70b1fe
David Rose <doppler@gmail.com>
2020-12-02 23:15:00 -0500

    Make header text dynamic

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,16 +6,25 @@ const Header: React.FC = () => {
   const location = useLocation();
   const history = useHistory();
   return (
-    <div>
-      {location.pathname === '/' ? (
-        <Link to="/menu">
-          <MenuButton />
-        </Link>
-      ) : (
-        <button onClick={() => history.goBack()} className="icon">
-          <BackButton />
-        </button>
-      )}
+    <div className="container">
+      <div>
+        {location.pathname === '/' ? (
+          <Link to="/menu">
+            <MenuButton />
+          </Link>
+        ) : (
+          <button onClick={() => history.goBack()} className="icon">
+            <BackButton />
+          </button>
+        )}
+      </div>
+      <div className="title">
+        {location.pathname === '/'
+          ? 'Winds Aloft'
+          : location.pathname
+              .replace(/^\//, '')
+              .replace(/[^]/, v => v.toUpperCase())}
+      </div>
     </div>
   );
 };
