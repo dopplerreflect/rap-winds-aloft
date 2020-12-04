9bfa881b884c8d21acdc5c7faf9ffcb6848aa188
David Rose <doppler@gmail.com>
2020-12-04 18:09:10 -0500

    fix dependency array

--- src/lib/useElevation.ts
+++ src/lib/useElevation.ts
@@ -38,6 +38,6 @@ export const useElevation = (
     fetchElevationData(location);
 
     return () => abortController.abort();
-  }, [location]);
+  }, [location, elevation, setStatus]);
   return elevation;
 };
