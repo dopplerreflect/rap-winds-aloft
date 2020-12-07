05405305e2fcf5e44b03d3d6bd3e9c9129059a8a
David Rose <doppler@gmail.com>
2020-12-07 17:58:16 -0500

    wrap in describe()

--- src/components/WindsAloft.test.tsx
+++ src/components/WindsAloft.test.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import WindsAloft from './WindsAloft';
 
-test('renders Winds Aloft starting', async () => {
-  render(<WindsAloft />);
-  const text = screen.getByText(/Loading\.\.\./);
-  expect(text).toBeInTheDocument();
-  expect(navigator.geolocation.getCurrentPosition).toBeCalledWith(
-    expect.any(Function)
-  );
+describe('testing WindsAloft', () => {
+  test('renders Winds Aloft starting', async () => {
+    render(<WindsAloft />);
+    const text = screen.getByText(/Loading\.\.\./);
+    expect(text).toBeInTheDocument();
+    expect(navigator.geolocation.getCurrentPosition).toBeCalledWith(
+      expect.any(Function)
+    );
+  });
 });
