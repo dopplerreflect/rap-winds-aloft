6db53e177c1ed8cbf50f7c73ea76596e722b914b
David Rose <doppler@gmail.com>
2020-12-09 10:01:18 -0500

    Use Docker to run inkscape and imagemagick

--- script/create-pwa-icons.sh
+++ script/create-pwa-icons.sh
@@ -1,10 +1,16 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
+if [[ "$(docker images -q image-tools 2> /dev/null)" == "" ]]; then
+  echo "Creating image-tools Docker image"
+  docker build -t image-tools .
+fi
+
+docker run -it --rm -v `pwd`:`pwd` -w `pwd` image-tools /bin/ash -c "pwd && \
 inkscape \
-  --export-png=./public/logo.png \
+  --export-filename=./public/logo.png \
   --export-dpi=200 \
   --export-background-opacity=0 \
-  ./public/logo.svg
+  ./public/logo.svg && \
 
 for n in 32 72 96 128 144 152 192 384 512 \
 ; do 
@@ -12,8 +18,9 @@ for n in 32 72 96 128 144 152 192 384 512 \
   -define png:compression-filter=5 \
   -define png:compression-level=9 \
   -define png:compression-strategy=1 \
-  -resize ${n}x${n} \
-  ./public/logo-${n}x${n}.png \
+  -resize \${n}x\${n} \
+  ./public/logo-\${n}x\${n}.png \
 ; done
+"
 
-mv ./public/logo-32x32.png ./public/favicon.png
+# mv ./public/logo-32x32.png ./public/favicon.png
